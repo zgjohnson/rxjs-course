b61c93a1afd31e08a1f4c4bb104f6db2ae777045
johnszg <zac.johnson@dillards.com>
2024-02-09 14:30:48 -0600

    implementing an observer from scratch

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {noop, observable, Observable} from "rxjs";
+import {createHttpObservable} from "../common/util";
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'about',
@@ -7,9 +10,50 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+
+    // Stream of Values examples (interval over time)
+    //   document.addEventListener('click', event => {
+    //     console.log(event)
+    //   })
+    //   let counter = 0;
+    //   setInterval(() => {
+    //     console.log(counter);
+    //     counter++;
+    //   }, 1000);
+    //
+    //   setTimeout(() => {
+    //     console.log("Finished.....");
+    //   }, 3000);
+
+    // Example with RxJs
+    // blueprint for the stream
+    // const interval$ = interval(1000)
+
+    // An Observable only becomes a stream once it's subscribed to.
+    // interval$.subscribe(value => console.log("stream 1 " + value));
+    //
+    // interval$.subscribe(value => console.log("stream 2 " + value));
+
+    // Subscriptions and Observables
+    // const interval$ = timer(3000, 1000);
+    // const sub = interval$.subscribe(value => console.log("stream 1 " + value));
+    //
+    // setTimeout(() => sub.unsubscribe(), 5000);
+    //
+    // const click$ = fromEvent(document, 'click');
+    //
+    // click$.subscribe(event => console.log(event),
+    //   error => console.log(error),
+    //   () => console.log("completed"));
+
+
+
+  }
+
+
 }
+
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {Course} from "../model/course";
-import {interval, Observable, of, timer} from 'rxjs';
+import {interval, noop, Observable, of, timer} from 'rxjs';
 import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
+import {createHttpObservable} from "../common/util";
 
 
 @Component({
@@ -12,12 +13,33 @@ import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/oper
 export class HomeComponent implements OnInit {
 
 
+    beginnerCourses: Course[];
+    advancedCourses: Course[];
+
     constructor() {
 
     }
 
     ngOnInit() {
+      // http observable from scratch
 
+
+      //Making my own observable
+      const https$ = createHttpObservable('/api/courses')
+
+      const courses$ = https$
+        .pipe(
+          map(res => Object.values<Course>(res["payload"]))
+        )
+
+      courses$.subscribe(
+        courses => {
+          this.beginnerCourses = courses.filter(course => course.category == 'BEGINNER');
+          this.advancedCourses = courses.filter(course => course.category == 'ADVANCED');
+        },
+        noop,
+        () => console.log('completed')
+      )
 
 
     }
